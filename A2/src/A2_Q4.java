830f48a5898b829442cdd1e0eda4493b815c7b93
letaoli123 <ericletao@yahoo.ca>
2023-03-11 18:39:17 -0500

    survive

--- A2/src/A2_Q4.java
+++ A2/src/A2_Q4.java
@@ -3,7 +3,20 @@ import java.util.*;
 public class A2_Q4 {
 	
 	public static double swaps(int[] passengers) {
+
 		return 0;
 	}
-	
+	public static double MergeSort(int[] passengers, int start, int end){
+		
+		if(start < end){
+			int half = (start+end)/2;
+			MergeSort(passengers, start, half);
+			MergeSort(passengers, half+1, end);
+			Merge(passengers, start, half, end);
+		}
+	}
+
+	public static int Merge(int[] passengers, int start, int half, int end){
+
+	}
 }
