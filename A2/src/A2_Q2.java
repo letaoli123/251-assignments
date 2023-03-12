cdd328e092012319729f1aba4b6330329e32f95f
letaoli123 <ericletao@yahoo.ca>
2023-03-12 19:29:28 -0400

    SU

--- A2/src/A2_Q2.java
+++ A2/src/A2_Q2.java
@@ -1,134 +1,46 @@
-import java.util.*;
-
 public class A2_Q2 {
 
 	public static int change(int[] denominations) {
-		int[] changeDP = changeListDP(denominations);
-		int[] changeGreedy = changeListGreedy(denominations);
+		int listLength = denominations[denominations.length - 1] + denominations[denominations.length - 2]+1;
+		int[] result = new int[listLength];
 
-		if(changeDP.equals(changeGreedy)){
-			return -1;
-		}else {
-			for (int i = 1; i < changeDP.length; i++) {
-				if (changeDP[i] < changeGreedy[i]) {
-					return i;
-				}
+		for(int i = 1; i<listLength; i++){
+			int changeDP = changeListDP(denominations, result, i);
+			int changeGreedy = changeListGreedy(denominations, i);
+			if(changeDP < changeGreedy){
+				return i;
 			}
 		}
 		return -1;
 	}
-	public static int[] changeListDP(int[] denominations) {
-		int listLength = denominations[denominations.length - 1] + denominations[denominations.length - 2]+1;
-		int[] result = new int[listLength];
-		for(int i = 1; i<result.length; i++){
-			result[i] = (denominations[denominations.length - 1])+(denominations[denominations.length-2]);
-		}
-		result[0] = 0;
-
-		for(int i = 1; i<result.length; i++){
-			for(int j = 0; j<denominations.length; j++){
-				if(denominations[j]<=i){
-					result[i]= Math.min(result[i], result[i-denominations[j]]+1);
-				}
+	public static int changeListDP(int[] denominations, int[] result, int i) {
+		for(int j = 0; j<denominations.length; j++){
+			if(result[i]==0){
+				result[i] = denominations[denominations.length-1]+denominations[denominations.length-2];
+			}
+			if(denominations[j]<=i){
+				result[i]= Math.min(result[i], result[i-denominations[j]]+1);
 			}
 		}
-
-
-
-		return result;
-//		for (int i=1; i<result.length; i++){
-//			result[i] = new ArrayList<Integer>();
-//
-//		}
-////		result[result.length-1].add(denominations[denominations.length-1]);
-////		result[result.length-1].add(denominations[denominations.length-2]);
-//
-//		for (int i = 0; i < denominations.length; i++) {
-//			result[denominations[i]].clear();
-//			result[denominations[i]].add(denominations[i]);
-//			for (int j = i+1; j<denominations.length; j++){
-//				if(result[denominations[i]+denominations[j]].isEmpty()) {
-//					result[denominations[i] + denominations[j]].add(denominations[j]);
-//					result[denominations[i] + denominations[j]].add(denominations[i]);
-//				}
-//			}
-//		}
-//
-//
-//		for(int i = 1; i < result.length; i++){
-//			if (result[i].isEmpty()){
-//				int amountOwed = i;
-//				for(int j = denominations.length-1; j>=0; j--){	//Check if the index is a multiple of a denomination
-//					if((amountOwed % denominations[j]) == 0){
-////						boolean noNeed = false;
-//						for(int k = denominations.length-1; k>j; k--){
-//							if(denominations[k] < i && denominations[k] > denominations[j]){
-//								ArrayList<Integer> tempArray = new ArrayList<>();
-//								if(i-1 > 0){
-//
-//
-//									tempArray.addAll(result[i-1]);
-//									amountOwed = amountOwed - (i-1);
-//									if(Arrays.asList(denominations).contains(amountOwed)){
-//										tempArray.add(amountOwed);
-//										amountOwed = amountOwed - amountOwed;
-//									}
-//									else{
-//										tempArray.add(1);
-//										amountOwed = amountOwed - 1;
-//									}
-////									tempArray.add(1);
-////									amountOwed = amountOwed - 1;
-//									result[i] = tempArray;
-//									break;
-//
-//								}
-//							}
-//							//else{
-////								while(amountOwed > 0){
-////									result[i].add(denominations[j]);
-////									amountOwed = amountOwed - denominations[j];
-////								}
-////								break;
-////							}
-//						}
-//						while(amountOwed > 0){
-//							result[i].add(denominations[j]);
-//							amountOwed = amountOwed - denominations[j];
-//						}
-//						break;
-//					}
-////					if(amountOwed == 0){
-////						break;
-////					}
-//				}
-//			}
-//
-//
-//		}
-
-
+		return result[i];
 	}
-		public static int[] changeListGreedy(int[] denominations) {
-			int listLength = denominations[denominations.length - 1] + denominations[denominations.length - 2]+1;
-			int[] result = new int[listLength];
-			for(int i = 1; i<result.length; i++){
-				int amountOwed = i;
-				while(amountOwed > 0){
-					for(int j = denominations.length-1; j >=0; j--){
-						if(denominations[j] <= amountOwed){
-							amountOwed = amountOwed-denominations[j];
-							result[i]++;
-							break;
-						}
-					}
-				}
+	public static int changeListGreedy(int[] denominations, int i) {
+
+		int amountOwed = i;
+		int coinNumbers = 0;
+
+		for(int j = denominations.length-1; j >=0; j--){
+			if(denominations[j] <= amountOwed){
+				coinNumbers = coinNumbers + amountOwed/denominations[j];
+				amountOwed = amountOwed % denominations[j];
+
 			}
 
-			return result;
 		}
 
-		public static String toString(int[] arrayLists){
+		return coinNumbers;
+	}
+	public static String toString(int[] arrayLists){
 		String string = "";
 		for(int i = 1; i<arrayLists.length; i++) {
 			string = string + i + ": " + arrayLists[i] + "\n";
@@ -136,11 +48,11 @@ public class A2_Q2 {
 		return string;
 	}
 	public static void main(String[] args) {
-		int[] denominations = {1, 9999};
-		int[] change= changeListDP(denominations);
-		System.out.println(toString(change));
-		int[] greedyChange = changeListGreedy(denominations);
-		System.out.println(toString(greedyChange));
+		int[] denominations = {1, 10,13};
+//		int[] change= changeListDP(denominations);
+//		System.out.println(toString(change));
+//		int[] greedyChange = changeListGreedy(denominations);
+//		System.out.println(toString(greedyChange));
 
 		int counterExample = change(denominations);
 		System.out.println(counterExample);
--- A2/src/A2_Q4.java
+++ A2/src/A2_Q4.java
@@ -1,22 +1,66 @@
 import java.util.*;
 
 public class A2_Q4 {
-	
-	public static double swaps(int[] passengers) {
 
-		return 0;
+	public static double swaps(int[] passengers) {
+		int start = 0;
+		int end = passengers.length-1;
+		double swaps = MergeSort(passengers, start, end);
+		return swaps;
 	}
 	public static double MergeSort(int[] passengers, int start, int end){
-		
+		double totalSwaps = 0;
 		if(start < end){
 			int half = (start+end)/2;
-			MergeSort(passengers, start, half);
-			MergeSort(passengers, half+1, end);
-			Merge(passengers, start, half, end);
+			double sort1 = MergeSort(passengers, start, half);
+			double sort2 = MergeSort(passengers, half+1, end);
+
+			double mergeSwap = Merge(passengers, start, half, end);
+			totalSwaps = sort1+sort2+mergeSwap;
 		}
+		return totalSwaps;
 	}
 
-	public static int Merge(int[] passengers, int start, int half, int end){
+	public static double Merge(int[] passengers, int start, int half, int end){
+		double mergeSwap = 0;
+		int n1 = half-start+1;
+		int n2 = end - half;
+		int[] leftArray = new int[n1];
+		int[] rightArray = new int[n2];
+		for(int i = 0; i<n1; i++){
+			leftArray[i] = passengers[start+i];
+		}
+		for(int i = 0; i<n2; i++){
+			rightArray[i] = passengers[half+1+i];
+		}
+		int leftIndex = 0;
+		int rightIndex = 0;
+		for(int i = start; i<=end; i++){
+			if(leftIndex >= leftArray.length) {
+				passengers[i] = rightArray[rightIndex];
+				rightIndex++;
+				continue;
+			}else if(rightIndex >= rightArray.length) {
+				passengers[i] = leftArray[leftIndex];
+				leftIndex++;
+				continue;
+			}
+			if(leftArray[leftIndex] <= rightArray[rightIndex]){
+				passengers[i] = leftArray[leftIndex];
+				leftIndex++;
+			}else{
+				passengers[i] = rightArray[rightIndex];
+				rightIndex++;
+				mergeSwap += leftArray.length - leftIndex;
+			}
+		}
+		return mergeSwap;
+	}
 
+	public static void main(String[] args) {
+
+		int[] passengers = {12,4,65,3};
+		double swaps = MergeSort(passengers, 0, passengers.length-1);
+		System.out.println(swaps);
 	}
 }
